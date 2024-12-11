1a1f001477a06def888d256e1ae42916ba54358e
brosgor <alfonso.suarez.dev@gmail.com>
2024-12-10 20:32:55 -0500

    added:

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const helloRoutes = require('./modules/hello/routes');
-
+const departments = require('./modules/departments/routes');
 const app = express();
 
 app.use(express.json());
 
 // Registrar la ruta principal para "Hello World"
 app.use('/api/hello', helloRoutes);
-
+app.use('/api/departments', departments);
 module.exports = app;
