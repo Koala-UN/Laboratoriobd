af3dea11432a9622ffe8c81e948ab8602a2dfe23
hmayorgav <hmayorgav@unal.edu.co>
2024-12-14 09:04:43 -0500

    haider - cambios cors

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ const helloRoutes = require('./modules/hello/routes');
 const departments = require('./modules/departments/routes');
 const municipalities = require('./modules/municipalities/routes');
 const zone = require('./modules/zone/routes');
+const cors = require('cors');   
+const people = require('./modules/people/routes');
+
 const houses = require('./modules/houses/routes');
 const app = express();
+app.use(cors());
 
 
 app.use(express.json());
@@ -15,5 +19,6 @@ app.use('/api/departments', departments);
 app.use('/api/municipalities', municipalities);
 app.use('/api/zone',zone)
 app.use('/api/houses', houses);
+app.use('/api/people', people);
 
 module.exports = app;
